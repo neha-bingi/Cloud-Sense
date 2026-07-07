c1427d7c2a8fa3a5e12997a77c29f9adf99359a4
neha-bingi <nehabingi4@gmail.com>
2026-07-07 21:11:51 +0530

    Update CORS configuration

--- cloudsense-backend/app/main.py
+++ cloudsense-backend/app/main.py
@@ -8,11 +8,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "https://cloud-sense-three.vercel.app",
-    ],
-    allow_credentials=True,
+    allow_origin_regex=r"https://.*\.vercel\.app",
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
